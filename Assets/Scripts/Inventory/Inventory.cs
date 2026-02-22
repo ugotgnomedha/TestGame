d73dfca7bbcd761da3361fcc2d4d7b4c4f3dfcda
PeteGk123 <triplerocketgr@gmail.com>
2026-02-22 11:08:54 +0200

    fix

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -6,6 +6,7 @@ namespace InventorySystem
     public class Inventory : MonoBehaviour
     {
         [SerializeField] private Transform hand;
+        [SerializeField] private Transform dropPos;
         private GameObject equipedItem;
         private BaseItemData equipedData;
         private bool isEquiped;
@@ -43,7 +44,7 @@ namespace InventorySystem
             GameObject itemToDrop = null;
             if (isEquiped)
             {
-                itemToDrop = Instantiate(equipedData.DropPrefab, hand.transform.position,Quaternion.identity);
+                itemToDrop = Instantiate(equipedData.DropPrefab, dropPos.transform.position,Quaternion.identity);
                 Destroy(equipedItem.gameObject);
                 equipedItem = null;
                 equipedData = null;
